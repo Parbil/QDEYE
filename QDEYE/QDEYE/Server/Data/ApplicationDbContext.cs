ecc3b90054db9391c42c39bded1a7b8663b0cb4b
Anwar Parbil <anwarp.net@gmail.com>
2020-06-22 12:00:06 +0530

    Inital Project

---
+++ QDEYE/QDEYE/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using QDEYE.Server.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QDEYE.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                var table = entityType.GetTableName();
+                if (table.StartsWith("AspNet"))
+                {
+                    table=table.Substring(6);
+                }
+                entityType.SetTableName(table.ToString());
+            }
+        }
+    }
+}
