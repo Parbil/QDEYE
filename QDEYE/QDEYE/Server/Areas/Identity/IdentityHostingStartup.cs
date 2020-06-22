19fbe4c8160137fc2ac4ab389b095fbd75742dbd
Anwar Parbil <anwarp.net@gmail.com>
2020-06-22 12:45:54 +0530

    Identtity Scaffolding

---
+++ QDEYE/QDEYE/Server/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using QDEYE.Server.Data;
+using QDEYE.Server.Models;
+
+[assembly: HostingStartup(typeof(QDEYE.Server.Areas.Identity.IdentityHostingStartup))]
+namespace QDEYE.Server.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
